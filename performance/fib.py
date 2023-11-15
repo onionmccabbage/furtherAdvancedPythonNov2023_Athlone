c01d715e8fcd4f13b61de1d8cd0bf0206ce6d85f
onionmccabbage <onionmccabbage@live.com>
2023-11-15 13:59:56 +0000

    using cProfile

--- performance/fib.py
+++ performance/fib.py
@@ -1,5 +1,13 @@
 # the Fibonacci sequence is a classic mathematical problem
 import timeit
+# profile is useful to see what our function is using (time, memory)
+from memory_profiler import profile
+
+# Python includes a profiler tool called cProfile
+# use it like this:
+# python -m cProfile -o profile_output fib.py
+# this will generate a non-text file called profile_output
+# We then write further Python code to read that file
 
 def fib(n):
     '''calculate the fibonacci number from n'''
@@ -10,13 +18,15 @@ def fib(n):
         # apply fib recursively
         return ( fib(n-1) + (fib(n-2)) )
     
+# @profile
 def main():
-    n=8
+    # n=40 # about 30 sec on my laptop
+    n=16
     r = fib(n) # 34
     print(f'Fibonacci up to {n} is {r}')
 
 if __name__ == '__main__':
     start = timeit.default_timer()
-    main()
+    main() # this will call the progile decorator
     end = timeit.default_timer()
     print(f'Total time {end-start}')

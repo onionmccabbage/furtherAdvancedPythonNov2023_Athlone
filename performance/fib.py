a833dc1c1edd62d70ee1925a59b7936104be2f2e
onionmccabbage <onionmccabbage@live.com>
2023-11-15 16:21:32 +0000

    samples with review3 solutions (fib and weather)

--- performance/fib.py
+++ performance/fib.py
@@ -2,6 +2,8 @@
 import timeit
 # profile is useful to see what our function is using (time, memory)
 from memory_profiler import profile
+# for functional programming there are some handy tools
+from functools import reduce
 
 # Python includes a profiler tool called cProfile
 # use it like this:
@@ -9,7 +11,7 @@ from memory_profiler import profile
 # this will generate a non-text file called profile_output
 # We then write further Python code to read that file
 
-def fib(n):
+def fib(n): # this is probably the most inefficent way to do it
     '''calculate the fibonacci number from n'''
     # 1, 2, 3, 5, 8, 13, 21 ...
     if n<=1:
@@ -18,12 +20,29 @@ def fib(n):
         # apply fib recursively
         return ( fib(n-1) + (fib(n-2)) )
     
+def fib_2(n):
+    '''a more performant solution'''
+    sequence = (0,1)
+    for _ in range(2, n+2): # remember we need to return the sum of the last TWO members
+        '''repeatedly add the last two values of the sequence'''
+        # NB here we have a one-member tuple (so remember the trailing comma)
+        sequence += (reduce( lambda a,b: a+b, sequence[-2:] ),) # works for a tuple
+    # we must return
+    return sequence[-1] # the last member
+
+def fib_3(n): # on my laptop this is fastest
+    a, b = 0, 1
+    for _ in range(n):
+        a, b = b, a+b # this is classic stateful - destroy prev state replace with new state
+    return a
+
 # @profile
 def main():
     # n=40 # about 30 sec on my laptop
-    n=16
-    r = fib(n) # 34
-    print(f'Fibonacci up to {n} is {r}')
+    n=1024
+    # r1 = fib_2(n) # Total time 0.0021428000000000003
+    r2 = fib_3(n) # Total time 0.0006612000000000284
+    print(f'Fibonacci up to {n} is {r2}')
 
 if __name__ == '__main__':
     start = timeit.default_timer()

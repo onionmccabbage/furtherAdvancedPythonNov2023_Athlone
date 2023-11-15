9487362eaae28f3c103a2b1bc3ffcd09334964e1
onionmccabbage <onionmccabbage@live.com>
2023-11-15 11:47:58 +0000

    using decorator to apply lock to class methods

--- threading_eg/using_thread_decorator.py
+++ threading_eg/using_thread_decorator.py
@@ -21,12 +21,32 @@ def lockMethod(meth):
     lockedMethod.__is_locked = True # a marker to indicate the method is locked
     return lockedMethod
 
+def make_thread_safe(cls, meth_l, lock):
+    '''take a method list and apply locks to ach method in that list'''
+    init = cls.__init__ # take a copy of the original __init__ method
+    def new_init(self, *args, **kwargs):
+        init(self, *args, **kwargs)
+        self.__lock = lock
+    cls.__init__ = new_init # make our new init the __init__ of our class
+    # iterate over each method in the method list and apply lock
+    for meth in meth_l:
+        old_meth = getattr(cls, meth)
+        new_meth = lockMethod(old_meth)
+        setattr(cls, meth, new_meth) # replace the orignal method with our locked version
+    return cls
+
+# in order to use as a decorator we need...
+def lockClass(meth_list, lock):
+    return lambda cls: make_thread_safe(cls, meth_list, lock)
+
+# we will decorate this class to lock the 'add' method
+@lockClass(['add', 'remove'], Lock) # both 'add' and 'remvoe' are now thread-safe
 class MySet(set): # this class inherits from 'set'
     '''this class extends the 'set' object. 'set' has 'add' and 'remove' '''
     def __init__(self, new_set):
         set.__init__(self, new_set) # just make a normal set
     # here is a method we may wish to lock
-    @lockMethod
+    # @lockMethod
     def myMethod(self, new_value):
         '''check that the new value is an integer'''
         if type(new_value) == int:
@@ -37,9 +57,11 @@ class MySet(set): # this class inherits from 'set'
 def main():
     ms = MySet({3,2,5,5,7,3,8,4,-34,-6,0,2})
     ms.myMethod(-3) # all good
+    ms.add('fine') # this works, since 'add' allows any data type
     ms.myMethod('oops') # fails silently
+    ms.remove(4) # all good
     print(ms) # does not include 'oops'
-    print(ms.myMethod.__is_locked) # True
+    # print(ms.myMethod.__is_locked) # True
 
 if __name__ == '__main__':
     main()

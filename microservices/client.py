acc2bd663109e087c67c437386cabeb00f8c45ba
onionmccabbage <onionmccabbage@live.com>
2023-11-17 10:36:51 +0000

    client using system argument variables

--- microservices/client.py
+++ microservices/client.py
@@ -1,5 +1,5 @@
 # a client makes requests to a service
-
+import sys
 import socket
 
 def myClient():
@@ -7,7 +7,11 @@ def myClient():
     cli = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # match the server
     config_t = ('127.0.0.1', 9876)
     cli.connect(config_t) # match the server
-    msg = 'Quail'
+    # if system arguments were provided, concatenate theminto a request string
+    if len(sys.argv)>1:
+        msg = ', '.join(sys.argv[1:]) # ignore the file name
+    else:
+        msg = 'Default'
     cli.send(msg.encode()) # always encode for http
     # handle any response
     res = cli.recv(1024)
---
+++ microservices/using_sysarg.py
@@ -0,0 +1,4 @@
+import sys
+
+# system argument variables are always strings
+print(sys.argv) # always contains the name of the running module
